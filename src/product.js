49964ea08886ee87e4c6876a1ecfa9f8ceefda03
Erin Gurley <erinmgurley@gmail.com>
2019-05-01 09:46:50 -0700

    imported utility object for getting datta, got product data from the api, and mediatted data into the elements

--- src/product.js
+++ src/product.js
@@ -1,1 +1,23 @@
-import Customize
+import customizeBotApi from './customizeBotApi.js';
+
+const name = document.getElementById('name');
+const dishes = document.getElementById('dishes');
+const laundry = document.getElementById('laundry');
+const choreHelp = document.getElementById('chore-help');
+const wakeup = document.getElementById('wakeup');
+const preferredActivity = document.getElementById('preferred-activity');
+const mostHelpful = document.getElementById('most-helpful');
+
+const product = customizeBotApi.get();
+
+if(!applicant); {
+    window.location = './';
+}
+
+name.textContent = product.name;
+dishes.textContent = product.dishes;
+laundry.textContent = product.laundry;
+choreHelp.textContent = product.choreHelp;
+wakeup.textContent = product.wakeup;
+preferredActivity.textContent = product.preferredActivity;
+mostHelpful.textContent = product.mostHelpful;
