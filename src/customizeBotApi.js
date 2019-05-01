4478049c093a136d36768b5c29e4bfc4bfa91b00
Erin Gurley <erinmgurley@gmail.com>
2019-04-30 17:24:16 -0700

    fixed function name

--- src/customizeBotApi.js
+++ src/customizeBotApi.js
@@ -1,18 +1,18 @@
-const customizeBot = {
+const customizeBotApi = {
     save(customizeBot) {
         //serialize to json
         const json = JSON.stringify(customizeBot);
         //save to local storage
-        localStorage.setItem('customizeBot',json);
+        localStorage.setItem('customizeBot', json);
     },
-    get( {
+    get() {
         //get from local storage
         const json = localStorage.getItem('customizeBot');
         // deserialize to object
         const customizeBot = JSON.parse(json);
         //return it
         return customizeBot;
-    });
-}
+    }
+};
 
 export default customizeBotApi;
