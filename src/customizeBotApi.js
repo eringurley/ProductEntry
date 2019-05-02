8339093f7d32e559727077592a83132889163ce3
Erin Gurley <erinmgurley@gmail.com>
2019-05-01 17:51:08 -0700

    reworeked API utility object to store list

--- src/customizeBotApi.js
+++ src/customizeBotApi.js
@@ -1,17 +1,22 @@
 const customizeBotApi = {
     save(customizeBot) {
+        //create array with applicant
+        const customizeBots = [];
+        customizeBots.push(customizeBot);
         //serialize to json
-        const json = JSON.stringify(customizeBot);
+        const json = JSON.stringify(customizeBots);
         //save to local storage
-        localStorage.setItem('customizeBot', json);
+        localStorage.setItem('customizeBots', json);
     },
     get() {
         //get from local storage
-        const json = localStorage.getItem('customizeBot');
+        const json = localStorage.getItem('customizeBots');
+        console.log(json);
         // deserialize to object
         const customizeBot = JSON.parse(json);
+        console.log(customizeBot);
         //return it
-        return customizeBot;
+        return customizeBot[0];
     }
 };
 
