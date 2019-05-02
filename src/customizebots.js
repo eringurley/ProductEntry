0d2574cd87717e006f4582758f5d4bc4589b81d7
Erin Gurley <erinmgurley@gmail.com>
2019-05-02 12:07:49 -0700

    ammended custmize bots.js file to correctly get populate table

--- src/customizebots.js
+++ src/customizebots.js
@@ -1,29 +1,33 @@
 import customizeBotApi from './customizeBotApi.js';
 
-const tbody = document.getElementById('customizeBots');
+const tbody = document.getElementById('customizedBots');
+console.log(tbody);
 
 const customizeBots = customizeBotApi.getAll();
 
 for(let i = 0; i < customizeBots.length;i++) {
     const customizeBot = customizeBots[i];
+    console.log(customizeBot);
 
     const tr = document.createElement('tr');
+    console.log(tr);
 
     const nameCell = document.createElement('td');
     nameCell.textContent = customizeBot.name;
-    tr.appendchild(nameCell);
+    tr.appendChild(nameCell);
 
     const dishesCell = document.createElement('td');
     dishesCell.textContent = customizeBot.dishes;
-    tr.appendchild(dishesCell);
+    tr.appendChild(dishesCell);
 
     const wakeupCell = document.createElement('td');
     wakeupCell.textContent = customizeBot.wakeup;
-    tr.appendchild(wakeupCell);
+    tr.appendChild(wakeupCell);
 
     const choreHelpCell = document.createElement('td');
     choreHelpCell.textContent = customizeBot.choreHelp;
-    tr.appendchild(choreHelpCell);
+    tr.appendChild(choreHelpCell);
+    console.log(tr)
 
     tbody.appendChild(tr);
 }
